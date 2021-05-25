aedcf995c74ed17c7b7c0d996c76398ba5263e1c
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-05-25 19:02:55 +0300

    Added dataset and data gathering script

---
+++ data/Pendigits.py
@@ -0,0 +1,32 @@
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+
+
+class Pendigits(Dataset):
+    def __init__(self, file_path: str):
+        lines = []
+        classes = []
+        with open(file_path, "r") as f:
+            for i, line in enumerate(f):
+                coordinates = list(map(int, line.rstrip().split(',')[:-1]))
+                line_class = int(line.split(',')[-1])
+                lines.append(coordinates)
+                classes.append(line_class)
+
+        self.classes = np.array(classes)
+        self.lines = np.array(lines)
+
+    def __len__(self):
+        return len(self.lines)
+
+    def __getitem__(self, item):
+        return self.lines[item], self.classes[item]
+
+
+if __name__ == '__main__':
+    ds = Pendigits('pendigits.tra')
+    dl = DataLoader(ds, num_workers=2, batch_size=10)
+    for idx, e in enumerate(dl):
+        l, c = e
+        print(c.shape)
+        print(l.shape)
---
+++ data/get_data.sh
@@ -0,0 +1,3 @@
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/pendigits/pendigits.tes
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/pendigits/pendigits.tra
+
